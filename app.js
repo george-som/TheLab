c6e91e53d3a31fdfd9c1c46f85ba263cdb158f75
georgesom <george.som@gmail.com>
2014-01-21 08:25:40 -0800

    Started laying out the template

---
+++ app.js
@@ -0,0 +1,35 @@
+(function(ng) {
+    ng.module("mi.repair.web", ['ngRoute', 'ngAnimate'])
+        .config(["$routeProvider", "$locationProvider",
+            function($routeProvider, $locationProvider) {
+
+                $routeProvider
+                    /*
+                    .when("/", {
+                        templateUrl: "views/job-overview/job-list.html",
+                        controller: "mi.repair.web.MainCtrl"
+
+                    })*/
+                    .when("/", {
+                        templateUrl: "views/job-overview/job-overview.html",
+                        controller: "mi.repair.web.MainCtrl"
+
+                    });
+
+                //$locationProvider.html5Mode(true);
+            }]);
+
+    ng.module("mi.repair.web")
+
+        .controller("mi.repair.web.MainCtrl", ['$scope', function($scope) {
+
+        }])
+        .directive("miShell", [function() {
+
+            return {
+                restrict: 'AE',
+                templateUrl: "components/shell/shell.html"
+                //controller: "mi.repair.web.ShellCtrl"
+            }
+        }]);
+}(angular))
